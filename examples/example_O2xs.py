eb457ebd58a8ebf716cdac78e40a4fb89ec34efa
stggh <Stephen.Gibson@anu.edu.au>
2016-07-03 12:59:35 +1000

    trim example to isolate unicode error 2

--- examples/example_O2xs.py
+++ examples/example_O2xs.py
@@ -19,18 +19,3 @@ d = 'potentials/'  # directory name
 
 wavelength = np.arange(110, 174.1, 0.1)  # nm
 
-# initialize CSE problem - any missing essential parameters are requested
-# mu - reduced mass
-# eni - initial state guess energy
-# VTI - initial state(s)
-# VTf - final coupled states
-# coupf - homogeneous coupling
-# dipolemoment - transition moments 
-
-X = cse.Xs(mu='O2', VTi=[d+'X3S-1.dat'], eni=800,
-                    VTf=[d+'B3S-1.dat', d+'3P1.dat', d+'E3S-1.dat',
-                         d+'3PR1.dat'],
-                    coupf=[40, 4000, 0, 0, 7000, 0],
-                    dipolemoment=[1, 0, 0, 0.3])
-                    #, transition_energy=wavelength)  # <--- alternative direct call
-
