739de7958cbe3c1ca03f8b1f462b7f4713e3d233
stggh <Stephen.Gibson@anu.edu.au>
2016-07-25 20:44:36 +1000

    example_O2X_rotation annotation label, error estimates for parameters

--- examples/example_O2X_rotation.py
+++ examples/example_O2X_rotation.py
@@ -4,6 +4,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 
 # evaluates E_J" vs J"(J"+1) for the O2 X-state
+
 X = cse.Cse('O2', VT=['potentials/X3S-1.dat'])
 
 X.solve(800)
@@ -14,21 +15,28 @@ B0 = X.Bv
 Jp = []
 EJ = []
 Jrange = np.arange(0, 30, 5)
-print('   J"     E"(cm-1)')
+print('    J"        E"(cm-1)')
 for J in Jrange:
      X.solve(E0+B0*J*(J+1), J)
-     print("    {:d}   {:g}".format(J, X.cm))
+     print("    {:2d}     {:8.3f}".format(J, X.cm))
      EJ.append(X.cm)
 
+EJ = np.array(EJ)
 JJ = Jrange*(Jrange+1)
 slope, intercept, r_value, p_value, std_err = stats.linregress(JJ, EJ)
+mx = EJ.mean()
+sx2 = ((EJ-mx)**2).sum()
+sd_intercept = std_err * np.sqrt(1/len(EJ) + mx*mx/sx2)
+sd_slope = std_err * np.sqrt(1/sx2)
 
-print("B={:g}, slope={:g}, intercept={:g}".format(B0, slope, intercept))
+print("B={:g}, slope={:9.7f}+-{:8.7f}, intercept={:9.7f}+-{:8.7f}"
+      .format(B0, slope, sd_slope, intercept, sd_intercept))
 
 
 plt.plot(JJ, EJ, 'ob')
 plt.plot(JJ, intercept+slope*JJ, 'r-')
 plt.xlabel("$J(J+1)$")
 plt.ylabel("energy (cm$^{-1}$)")
+plt.annotate("{:8.3f} + {:8.3f}J(J+1)".format(intercept, slope), (400, EJ.mean()))
 plt.axis(xmin=-1)
 plt.show()
