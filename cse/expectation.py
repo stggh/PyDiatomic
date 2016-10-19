c45e153e37c160f386f8d53dc2665553217128c4
stggh <Stephen.Gibson@anu.edu.au>
2016-10-19 18:52:30 +1100

    sync

--- cse/expectation.py
+++ cse/expectation.py
@@ -5,6 +5,7 @@ import scipy.constants as const
 from scipy import linalg
 from scipy.optimize import leastsq
 from scipy.integrate.quadrature import simps
+import itertools
 
 import multiprocessing
 from functools import partial
@@ -67,10 +68,11 @@ def cross_section(wavenumber, wfu, wfi, R, dipolemoment):
     return xsp
 
 
-def xs(dipolemoment, ei, rot, mu, R, VT, wfi, wavenumber):
+def xs(dipolemoment, ei, mu, R, VT, wfi, waverot):
     """ solve CSE of upper coupled states for the transition energy.
 
     """
+    wavenumber, rot = waverot
     dE = wavenumber/8065.541
     en = ei + dE
     wfu, eu = johnson.solveCSE(en, rot, mu, R, VT)
@@ -83,9 +85,9 @@ def xs_vs_wav(wavenumber, dipolemoment, ei, rot, mu, R, VT, wfi):
 
     """
     pool = multiprocessing.Pool()
-    func = partial(xs, dipolemoment, ei, rot, mu, R, VT, wfi)
+    func = partial(xs, dipolemoment, ei, mu, R, VT, wfi)
 
-    xsp = pool.map(func, wavenumber)
+    xsp = pool.map(func, itertools.chain(itertools.product(wavenumber, rot)))
     xsp = np.array(xsp)
     pool.close()
     pool.join()
---
+++ doc/example_rkr.rst
@@ -0,0 +1,5 @@
+Example: example_rkr.py
+=======================
+
+.. plot:: ../examples/example_rkr.py
+   :include-source:
