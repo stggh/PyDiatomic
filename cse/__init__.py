ee08009283766c56b3c739d2e420e18dfe1db012
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2016-11-03 16:16:51 +1100

    removed extra imports from __init__.py

--- cse/__init__.py
+++ cse/__init__.py
@@ -1,10 +1,5 @@
 from ._version import __version__
 
 from . import cse
-from . import johnson
-from . import cse_setup
-from . import expectation
-from . import rkr
-from . import analytical
 from .rkr import rkr
 from .cse import Cse, Xs
