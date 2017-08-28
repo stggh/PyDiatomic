90c64445c249e2344756e46c22b951ae3fb6d040
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2017-08-28 10:37:05 +1000

    updated HO example

--- examples/example_HO.py
+++ examples/example_HO.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cse
 import matplotlib.pyplot as plt
+from scipy.special import hermite
 
 ######################################################
 #
@@ -11,14 +12,14 @@ import matplotlib.pyplot as plt
 ######################################################
 
 
-def phi(n, R, alpha):
+def phi(v, R, alpha):
     y = R*np.sqrt(alpha)
-    if n == 0:
-        return (alpha/np.pi)**0.25 * np.exp(-y**2/2)
-    if n == 1:
-        return -(alpha/np.pi)**0.25 * np.sqrt(2) * y * np.exp(-y**2/2)
-    if n > 1:
-        return (alpha/np.pi)**0.25 * ((2*y**2-1)/np.sqrt(2)) * np.exp(-y**2/2)
+    Nv = (alpha/np.pi)**0.25/np.sqrt(2**v*np.math.factorial(v))
+    Hv = hermite(v)
+    sum = 0.0
+    for i, h in enumerate(Hv.coeffs[::-1]):
+        sum += h*(y**i)
+    return (-1)**v*sum*Nv*np.exp(-y**2/2)
 
 
 R = np.linspace(-2, 2, 2001)
@@ -27,18 +28,24 @@ V = R**2/10
 
 X = cse.Cse(2, VT=[(R, V)])
 
-for en in [0.01, 0.02, 0.04]:
-    X.solve(en)
-    print("E(v={:d}) = {:5.2f} cm-1".format(X.vib, X.cm))
-    plt.plot(R, X.wavefunction[:, 0, 0]*400 + X.cm,
-             label=r'$v={:d}$'.format(X.vib))
-    plt.plot(R, phi(X.vib, R, 10)*400 + X.cm, 'k--')
+fig, ax = plt.subplots()
 
-plt.plot(np.NaN, np.NaN, 'k--', label=r'analytical')
-plt.plot(R, V*8065.541)
-plt.axis(xmin=-1.5, xmax=1.5, ymin=-800, ymax=1600)
-plt.legend()
-plt.title("Harmonic oscillator potential")
-plt.ylabel(r"potential energy (cm$^{-1}$) / wavefunction $\times 400$")
-plt.xlabel(r"$r$")
+print(" v    E(v) in cm-1")
+for en in [0.01, 0.02, 0.04, 0.06, 0.08]:  # guess energies in eV
+    X.solve(en)
+    print("{:2d}  {:8.2f}".format(X.vib, X.cm))
+    ax.plot(R, X.wavefunction[:, 0, 0]*400 + X.cm,
+            label=r'$v={:d}$'.format(X.vib))
+    ax.plot(R, phi(X.vib, R, 10)*400 + X.cm, 'k--')
+
+ax.plot(np.NaN, np.NaN, 'k--', label=r'analytical')
+ax.plot(R, V*8065.541)
+ax.axis(xmin=-1.5, xmax=1.5, ymin=-800, ymax=1600)
+ax.legend()
+ax.set_title(r"Harmonic oscillator potential:"
+             r" $\psi_v(x) = N_v H_v(x) e^{-x^2/2}$")
+ax.set_ylabel(r"potential energy (cm$^{-1}$) / wavefunction $\times 400$")
+ax.set_xlabel(r"$x$")
+
+plt.savefig("output/example_HO.png", dpi=150)
 plt.show()
