24dc839e6986cfc969f88bfa6b607ffd77e8dadf
Stephen Gibson <Stephen.Gibsonm@anu.edu.au>
2017-09-06 06:19:38 +1000

    example_O2_fine_structure.py reduced calculation length

--- examples/example_O2_fine_structure.py
+++ examples/example_O2_fine_structure.py
@@ -12,13 +12,13 @@ O2e = cse.Cse('O2', VT=['potentials/X3S-1.dat', 'potentials/X3S0.dat',
 
 print("F1 levels")
 print("  cm-1      J      diff.")
-for en, J, F in en1:
+for en, J, F in en1[:10]:
     O2e.solve(en, rot=J)
     print("{:8.3f}   {:2.0f}   {:8.3f}".format(O2e.cm, J, O2e.cm-en))
 
 print("\nF3 levels")
 print("  cm-1      J      diff.")
-for en, J, F in en3:
+for en, J, F in en3[:10]:
     O2e.solve(en, rot=J)
     print("{:8.3f}   {:2.0f}   {:8.3f}".format(O2e.cm, J, O2e.cm-en))
 
@@ -27,6 +27,6 @@ for en, J, F in en3:
 O2f = cse.Cse('O2', VT=['potentials/X3S-1.dat'])
 print("\nF2 levels")
 print("  cm-1      J      diff.")
-for en, J, F in en2:
+for en, J, F in en2[:10]:
     O2f.solve(en, rot=J)
     print("{:8.3f}   {:2.0f}   {:8.3f}".format(O2f.cm, J, O2f.cm-en))
