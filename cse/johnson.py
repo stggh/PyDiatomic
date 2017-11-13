b18c762621b7c59b8f17c8f5fb1274923751df80
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2017-11-14 08:31:19 +1100

    rouille.py exact O2X fine strcuture levels

--- cse/johnson.py
+++ cse/johnson.py
@@ -350,6 +350,7 @@ def solveCSE(Cse, en):
     nopen = edash[openchann].size
 
     mx = matching_point(en, rot, V, R, mu, AM)
+    mx = 230
 
     if mx < oo-5:
         out = leastsq(eigen, (en, ), args=(rot, mx, V, R, mu, AM), xtol=0.01)
