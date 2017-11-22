f64ac5e488b088c904b3aef7a8e96e7ddfae21e8
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2017-11-22 11:33:12 +1100

    removed sympy dependency from travis => can no longer absolute import intensity.py

--- cse/__init__.py
+++ cse/__init__.py
@@ -4,5 +4,6 @@ from . import cse
 from .tools import analytical
 from .tools.rouille import rouille
 from .tools.RKR import rkr
-from .tools.intensity import Boltzmann, honl
+# take out due to sympy dependence
+#from .tools.intensity import Boltzmann, honl
 from .cse import Cse, Xs
