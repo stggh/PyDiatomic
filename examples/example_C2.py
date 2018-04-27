d6ff869b7a4a73f46a57349f0c2babe04bc8a7d6
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2018-04-27 15:35:54 +1000

    example_C2.py includes plot. Not sure why oscillator strength variation is not smooth

--- examples/example_C2.py
+++ examples/example_C2.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 #
 # (1) X1S0 - ground state RKR potential energy curve
 # (2) D1S0 - upper state RKR potential energy curve
-# (3) solve Schroedinger equation
+# (3) solve Schroedinger equation to evaluate Franck-Condon factors
 #
 ###################################################################
 
@@ -17,6 +17,8 @@ def G(v, we, wexe):
 
 def B(v, Be, alphae):
     return Be - alphae*(v+1/2)
+
+fig, (ax0, ax1) = plt.subplots(1, 2)
 
 # RKR potential energy curves
 # X1S0 ground state -----------
@@ -37,7 +39,10 @@ limb = 'L'
 print("X1S0 - ground state RKR ------------")
 RX, X1S0, RTP, PTP = cse.tools.RKR.rkr(mu, vv, Gv, Bv, De, limb, dv=0.1,
                                        Rgrid=np.arange(0.5, 10.001, 0.001))
-
+ax0.plot(RX, X1S0, label=r'$X ^1\Sigma_g^+$')
+ax0.plot(RTP[::10], PTP[::10], 'oC9')
+ax0.set_xlabel(r'internuclear distance ($\AA$)')
+ax0.set_ylabel(r'potential energy (eV)')
 
 # D1S0 upper state -----------
 
@@ -53,30 +58,46 @@ RD, D1S0, RTP, PTP = cse.tools.RKR.rkr(mu, vv, Gv, Bv, De, limb, dv=0.1,
 # Te displacement
 D1S0 += Te
 
+ax0.plot(RD, D1S0, 'C2', label=r'$D ^1\Sigma_g^+$')
+ax0.plot(RTP[::10], PTP[::10]+Te, 'oC8')
+ax0.axis(xmin=0.5, xmax=3, ymin=-0.5, ymax=8)
+
+# C2 D-X FCF calculation 
+#  published values Sorkhabi et al. J. Molec. Spectrosc. 188, 200-208 (1998)
+#  DXFCF[v", v']
+Sorkhabi_FCF = np.array([[0.9972, 2.72e-3, 8.807e-5, 3.676e-7, 5.04e-9],
+                         [2.642e-3, 0.9922, 4.842e-3, 2.692e-4, 1.929e-6],
+                         [1.644e-4, 4.547e-3, 0.9883, 6.447e-3, 5.494e-4],
+                         [5.995e-6, 4.596e-4, 5.846e-3, 0.9847, 8.00e-3],
+                         [2.59e-7, 1.76e-8, 0.001, 0.017, 0.9802]])
 # CSE calculation
 
 print("\nCSE FCF calculation -------------------")
-C2 = cse.Xs('C2', VTi=[(RX, X1S0)], eni=900, VTf=[(RD, D1S0)],
-            dipolemoment=[1])
+C2 = cse.Xs('C2', VTi=[(RX, X1S0)], VTf=[(RD, D1S0)], dipolemoment=[1])
 
 C2.gs.solve(1000)
-print('X(v"={:d}) = {:8.5f} cm-1'.format(C2.gs.vib, C2.gs.cm))
-
-C2.us.solve(43500)
-print('D(v\'={:d}) = {:8.5f} cm-1\n'.format(C2.us.vib, C2.us.cm))
+print('X(v"={:d}) = {:10.5f} cm-1'.format(C2.gs.vib, C2.gs.cm))
+ax0.plot(C2.gs.R, C2.gs.wavefunction.T[0, 0]/2+C2.gs.energy)
 
 # ground state eigenvalues - calculate every vibrational level = slow
 # C2.gs.levels(4)
 # enX = C2.gs.results[0][0]
 
-# upper state eigenvalues - all levels = slow
+# upper state eigenvalues - all levels = slow, but need the accurate 
+# print('calculating upper state eigenvalues, will take a little while ...')
 # C2.us.levels(4)
 # vibD = C2.us.results.keys()
 # enD = np.asarray(list(zip(*C2.us.results.values()))[0])
 
+C2.us.solve(43500)
+print('D(v\'={:d}) = {:10.5f} cm-1\n'.format(0, C2.us.cm))
+C2.us.solve(49500)
+ax0.plot(C2.us.R, C2.us.wavefunction.T[0, 0]/2+C2.us.energy)
+
 # or roughly known
 enD = np.array([44200, 45960, 47730, 49500, 51300, 55820, 63300, 80880])
 
+print('Franck-Condon calculation')
 print('v"    v\'    E\'-E"      FCF')
 for vdd in range(2):
     C2.gs.solve(1890*(vdd+1/2))
@@ -90,3 +111,9 @@ for vdd in range(2):
 
     for vib, (en, osc) in enumerate(zip(C2.wavenumber, fcf)):
         print('     {:2d}   {:8.3f}   {:7.2e}'.format(vib, en, osc))
+        p1 = ax1.semilogy(vib, osc, 'oC{}'.format(vdd))
+
+    p2 = ax1.plot(Sorkhabi_FCF[vdd], '+C{}'.format(9-vdd), ms=10)
+
+plt.savefig('output/example_C2.png', dpi=100)
+plt.show()
