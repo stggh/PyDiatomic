5bd2d29742acad483b0966041dcd4492934c43fe
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2019-01-03 12:14:54 +1100

    pytest - in circles

--- cse/__init__.py
+++ cse/__init__.py
@@ -1,9 +1,9 @@
 from ._version import __version__
 
 from . import cse
-# from .tools import analytical
-# from .tools.rouille import rouille
-# from .tools.xsT import total_cross_section
-# from .tools.RKR import rkr
-# from .tools.intensity import Boltzmann, honl, Wigner3j
+from .tools import analytical
+from .tools.rouille import rouille
+from .tools.xsT import total_cross_section
+from .tools.RKR import rkr
+from .tools.intensity import Boltzmann, honl, Wigner3j
 from .cse import Cse, Xs
