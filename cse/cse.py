fit bug where xs set to zero array of the wrong shape, for honl=0. Set xs shape to be (oo, n) rather than nopen. This provides for an increase in open channels for larger transition energies

cse/cse.py
@@ -358,6 +358,8 @@ class Xs():
 
         if honl:
             # Hönl-London factor J' J" Ω' Ω" for the main allowed transition
+            # fix me - this should be within expectation calculation
+            #          at the rotational matrix level
             self.honl = tools.intensity.honl(self.us.rot, self.gs.rot,
                                              self.us.AM[0][0], self.gs.AM[0][0])
             # self.eta = 
@@ -371,7 +373,7 @@ class Xs():
             self.wavenumber = np.array(self.wavenumber)
             self.nopen = self.xs.shape[-1] - 1
         else: # don't waste time on the calculation
-            self.xs = np.zeros((len(transition_energy), 1))
+            self.xs = np.zeros((len(transition_energy), self.us.VT.shape[0]))
 
 
     def align_grids(self):

cse/expectation.py
@@ -40,7 +40,7 @@ def cross_section(wavenumber, Xs):
 
     Returns
     -------
-    cross section : numpy array
+    cross section : numpy array - shape (oo, nopen)
         photodissociation cross section for each open channel
 
     """
@@ -57,7 +57,7 @@ def cross_section(wavenumber, Xs):
     for i in range(oo):
         overlap[i] = wfi[i] @ Xs.dipolemoment[i] @ Xs.us.wavefunction[i] 
 
-    xsp = np.zeros(nopen)  
+    xsp = np.zeros(n)   # allows later extra open channels   
     for j in range(nopen):
         Rx = simps(overlap[:, j].real, Xs.us.R)
         Ix = simps(overlap[:, j].imag, Xs.us.R)