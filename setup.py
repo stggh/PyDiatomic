7331ad3cc0a48d91de0a47d073d60e6cfcd8c382
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2022-02-24 16:10:03 +1100

    version to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "PyDiatomic",
-    version = "0.1",
+    version = "0.3",
     description='A Python package to solve the time-independent coupled-channel Schr&ouml;inger equation using the Johnson renormalized Numberov method',
     packages=find_packages(),
     classifiers=[
