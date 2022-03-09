49b6a7d29921edaef0738774b304c7062fb18a0b
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2022-03-10 10:27:01 +1100

    Wei analytical PEC function

--- cse/tools/analytical.py
+++ cse/tools/analytical.py
@@ -1,6 +1,40 @@
 # -*- coding: utf-8 -*
 import numpy as np
 
+def Wei(r, re, De, Te, b, h=0):
+    """ Modified Wei potential curve 
+           Jai et al. J Chem Phys 137, 014101 (2012).
+
+    Parameters
+    ----------
+    r : numpy 1d-array
+        internuclear distance grid
+    re : float
+        internuclear distance at equilibrium
+    De : float
+        Dissociation energy
+    Te : float
+        equilibrium energy (potential minimum)
+    b : float
+        anharmonicity parameter
+    h : float
+        |h| < 1, h=0 gives a Morse potential curve
+
+    Returns
+    -------
+    potential_curve : numpy 1d-array
+        potential energy curve
+
+    """
+
+    if abs(h) > 1:
+        raise SyetemExit(f'Wei(r, re, De, Te, b, h): error h={h}, but |h| < 1')
+        
+    ebre = np.exp(b*re)
+    ebr = np.exp(b*r)
+
+    return De*(1 - ebre*(1 - h)/(ebr - h*ebre))**2 + Te
+
 
 def Morse(r, re, De, Te, beta):
     """Morse potential energy curve.
@@ -25,7 +59,7 @@ def Morse(r, re, De, Te, beta):
 
     """
 
-    return De*(1 - np.exp(-beta*(r-re)))**2 + Te
+    return Wei(r, re, De, Te, beta, h=0)
 
 
 def Julienne(r, mx, rx, vx, voo):
