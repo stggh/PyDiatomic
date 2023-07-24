fd7c7057ce479904309fc873dc2bd88fe804027e
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2023-07-25 08:52:10 +1000

    Fix errors in pytest. Enhance test_model_fit.py.

--- cse/tests/test_cse.py
+++ cse/tests/test_cse.py
@@ -1,10 +1,12 @@
 import numpy as np
 import cse
+import os
 import numpy.testing as npt
 
 def test_O2X():
-    X = cse.Cse('O2', dirpath='../../examples/potentials', suffix='.dat',
-                VT=['X3S-1'])
+    path = os.path.dirname(__file__)
+    X = cse.Cse('O2', dirpath=path+'/../../examples/O2/potentials',
+                suffix='.dat', VT=['X3S-1'])
 
     # (Ω, S, Λ, Σ)
     npt.assert_equal(X.AM, [(1, 1, 0, 1, 1)])
@@ -12,32 +14,35 @@ def test_O2X():
 
     X.solve(800)
 
+    # X-state Tv, Bv, Dv for v=0
     npt.assert_almost_equal(X.cm, 787.40, decimal=2)
     npt.assert_almost_equal(X.Bv, 1.438, decimal=3)
     npt.assert_almost_equal(X.Dv, 4.84e-6, decimal=6)
 
 def test_transition():
-    X = cse.Cse('O2', dirpath='../../examples/potentials', suffix='.dat',
-                VT=['X3S-1'], en=800)
-    B = cse.Cse('O2', dirpath='../../examples/potentials', suffix='.dat',
-                VT=['B3S-1'])
+    path = os.path.dirname(__file__)
+    X = cse.Cse('O2', dirpath=path+'/../../examples/O2/potentials',
+                suffix='.dat', VT=['X3S-1'], en=800)
+    B = cse.Cse('O2', dirpath=path+'/../../examples/O2/potentials',
+                suffix='.dat', VT=['B3S-1'])
 
     BX = cse.Transition(B, X, dipolemoment=[1])
-    BX.calculate_xs(transition_energy=[51968,])
+    BX.calculate_xs(transition_energy=[51968,])  # bound-bound
 
-    npt.assert_almost_equal(BX.wavenumber, 51968.39, decimal=2)
+    npt.assert_almost_equal(BX.wavenumber, 51967.8, decimal=1)
 
 def test_coupled_channel():
-    X = cse.Cse('O2', dirpath='../../examples/potentials', suffix='.dat',
-                VT=['X3S-1'], en=800)
-    BE = cse.Cse('O2', dirpath='../../examples/potentials', suffix='.dat',
-                 VT=['B3S-1', 'E3S-1'], coup=[4000])
+    path = os.path.dirname(__file__)
+    X = cse.Cse('O2', dirpath=path+'/../../examples/O2/potentials',
+                suffix='.dat', VT=['X3S-1'], en=800)
+    BE = cse.Cse('O2', dirpath=path+'/../../examples/O2/potentials',
+                 suffix='.dat', VT=['B3S-1', 'E3S-1'], coup=[4000])
     BEX = cse.Transition(BE, X, dipolemoment=[1, 0])
 
     # O₂ Longest-band
-    BEX.calculate_xs(transition_energy=np.arange(81300, 81390, 4))
+    BEX.calculate_xs(transition_energy=np.arange(80950, 81050, 1))
 
-    npt.assert_almost_equal(BEX.wavenumber[BEX.xs[:, 0].argmax()], 81344,
+    npt.assert_almost_equal(BEX.wavenumber[BEX.xs[:, 0].argmax()], 81006,
                             decimal=0)
 
 if __name__ == '__main__':
--- cse/tests/test_model_fit.py
+++ cse/tests/test_model_fit.py
@@ -1,38 +1,84 @@
 import numpy as np
 import cse
 import numpy.testing as npt
+from scipy.interpolate import splrep, splev
 
-def test_ΔV():
-    iso = 'CO'
-
-    # CSE instances, ground and excited electronic states
-    X = cse.Cse(iso, VT=['../../examples/CO/potentials/X1S0.dat'])
-    B = cse.Cse(iso, VT=['../../examples/CO/potentials/B1S0.dat'])
-
-    # transition instance B <- X
+def CSEmodel(iso='CO'):
+    R = np.arange(0.9, 3, 0.01)
+    VX = cse.tools.analytical.Wei(R, Re=1.131, De=9.077e+04, Voo=90674, b=2.33,
+                                  h=-0.01314)
+    VB = cse.tools.analytical.Wei(R, Re=1.114, De=6.845e+04, Voo=155262.65,
+                                  b=3.1, h=-0.12)
+    X = cse.Cse(iso, VT=[(R, VX)])
+    B = cse.Cse(iso, VT=[(R, VB)])
     BX = cse.Transition(B, X, dipolemoment=[1])
 
-    B.levels(5)  # energy levels
+    BX.us.levels(5)
 
-    # levels, to fit, energy offset
+    return BX
+
+def test_ΔV():
+    BX = CSEmodel()
+    B = BX.us
+
     offset = 20
     v = np.arange(5)
-    Gv = []
-    for vv, (Tv, *_) in B.results.items():
-        if vv in v:
-            Gv.append(Tv + offset)  # off-set 'experimental levels'
+    Tv = np.array([x for (x, *_) in B.results.values()])[v] + offset
 
-
-    initval = 15  # starting offset
+    initval = 15
     lbl = B.statelabel[0] 
 
     fit = cse.tools.model_fit.Model_fit(BX, method='lm',
-                    data2fit={lbl:{'position':(v, Gv)}},  # fit data
+                    data2fit={lbl:{'position':(v, Tv)}},  # fit data
                     VT_adj={lbl:{'ΔV': initval}},         # PEC shift
                     verbose=False)
 
     scaling = fit.result.x[0]
     npt.assert_almost_equal(offset, scaling*initval, decimal=1)
 
+def test_Rstr(iso='CO'):
+    BX = CSEmodel()
+    X = BX.gs
+    B = BX.us
+    # B - unperturbed energy levels
+    v = np.arange(5)
+    Tv = np.array([x for (x, *_) in B.results.values()])[v]
+
+    # perturbation - widen the PEC around Re ----------------------
+    BV = B.VT[0, 0]
+    BR = B.R.copy()
+    Re = BR[BV.argmin()]
+
+    inner_perturb = 1.15
+    outer_perturb = 1.1
+    BR[BR < Re] = (BR[BR < Re] - Re)*inner_perturb + Re
+    BR[BR > Re] = (BR[BR > Re] - Re)*outer_perturb + Re
+    spl = splrep(BR, BV)
+    Bpert = splev(B.R, spl)  # a widened PEC
+
+    # new excited state instance, with widened PEC
+    Bp = cse.Cse(iso, VT=[(B.R, Bpert)])
+    Bp.levels(5)
+
+    lbl = B.statelabel[0]
+    Bp.statelabel[0] = lbl
+    # transition instance of perturbed PEC
+    BpX = cse.Transition(Bp, X, dipolemoment=[1])
+
+    # model_fit instance ------------------------------------------------
+    fit = cse.tools.model_fit.Model_fit(BpX, method='trf', bounds_factor=1,
+                          data2fit={lbl:{'position':(v, Tv)}},
+                          VT_adj={lbl:{'Rstr': {'inner':1, 'outer':1}}},
+                          verbose=False)
+
+    Bf = fit.csemodel.us
+    npt.assert_almost_equal(1/inner_perturb, inner_perturb*fit.result.x[0],
+                            decimal=1)
+    npt.assert_almost_equal(1/outer_perturb, outer_perturb*fit.result.x[1],
+                            decimal=1)
+
+
 if __name__ == '__main__':
+    BX = CSEmodel()
     test_ΔV()
+    test_Rstr()
